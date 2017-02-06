e8b815e148eeadfbd19c0ec92fa432d01c99217a
Alex Moore-Niemi <alex.moore-niemi@raizlabs.com>
2017-02-06 16:57:00 -0500

    output links

--- spec/acceptance/contacts_spec.rb
+++ spec/acceptance/contacts_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 require 'rspec_api_documentation/dsl'
+require 'uri'
 
 def jsonish_attributes(record)
   pairs = record.attributes.except("id", "created_at", "updated_at")
@@ -7,23 +8,45 @@ def jsonish_attributes(record)
   Hash[dash_keys.zip(pairs.values)]
 end
 
+def output_links
+  links_to = JSON.parse(response_body)['data'].
+               collect { |e| e['relationships'] }.
+               collect(&:keys).flatten
+
+  h = { "#{subject}" => links_to }
+  p h
+end
+
 resource 'Contacts' do
+  after(:each) do
+    output_links
+  end
   get '/contacts' do
     let!(:contact) { create(:contact) }
+    let(:contact_representation) {
+      {
+        data: [
+          {
+            id: "1",
+            links: {},
+            attributes: jsonish_attributes(contact),
+            relationships: {
+              "phone-numbers" => {
+                links: {
+                  self: URI.regexp,
+                  related: URI.regexp
+                }
+              }
+            }
+          }
+        ]
+      }
+    }
+
     example 'Listing contacts' do
       do_request
       expect(status).to eq(200)
-      expect(response_body).
-        to include_json(
-             data: [
-               {
-                 id: "1",
-                 links: {},
-                 attributes: jsonish_attributes(contact),
-                 relationships: {}
-               }
-             ]
-           )
+      expect(response_body).to include_json(contact_representation)
     end
   end
 end
